709a5678aa78626b5f75778c2a62807e20562b80
Aaronjohnroque <54198970+Aaronjohnroque@users.noreply.github.com>
2019-09-28 17:00:56 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/labactivity3_roqueaaron/MainActivity.java
+++ app/src/main/java/com/example/labactivity3_roqueaaron/MainActivity.java
@@ -14,23 +14,23 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    EditText input1, input2, input3, input4, input5, input6, input7;
+    EditText input1, input2, input3, input4, input5, input6, input7, input8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        input1 = findViewById(R.id.editText);
-        input2 = findViewById(R.id.editText2);
-        input3 = findViewById(R.id.editText3);
-        input4 = findViewById(R.id.editText4);
-        input5 = findViewById(R.id.editText5);
-        input6 = findViewById(R.id.editText6);
-        input7 = findViewById(R.id.editText7);
-
+        input1 =findViewById(R.id.editText);
+        input2 =findViewById(R.id.editText2);
+        input3 =findViewById(R.id.editText3);
+        input4 =findViewById(R.id.editText4);
+        input5 =findViewById(R.id.editText5);
+        input6 =findViewById(R.id.editText6);
+        input7 =findViewById(R.id.editText7);
+        input8 =findViewById(R.id.editText8);
     }
 
-    public void saveData(View v) {
+    public void saveData(View v){
         String data1 = input1.getText().toString();
         String data2 = input2.getText().toString();
         String data3 = input3.getText().toString();
@@ -38,33 +38,37 @@ public class MainActivity extends AppCompatActivity {
         String data5 = input5.getText().toString();
         String data6 = input6.getText().toString();
         String data7 = input7.getText().toString();
-        FileOutputStream writer = null;
+        String data8 = input8.getText().toString();
+
+        String dataAll = data1 + "," + data2 + "," + data3 + "," + data4 + "," + data5 + "," +data6+ "," +data7+ "," +data8;
+
+        String data = dataAll;
+        /**/        FileOutputStream writer = null;
         try {
             writer = openFileOutput("data1.txt", MODE_PRIVATE);
-            writer.write(data1.getBytes());
-            writer.write(data2.getBytes());
-            writer.write(data3.getBytes());
-            writer.write(data4.getBytes());
-            writer.write(data5.getBytes());
-            writer.write(data6.getBytes());
-            writer.write(data7.getBytes());
+            writer.write(data.getBytes());
 
         } catch (FileNotFoundException e) {
-            Log.d("error", "file not found...");
-        } catch (IOException e) {
-            Log.d("error", "IO eroor..");
-        } finally {
+            Log.d("Error", "File not found...");
+        }
+        catch (IOException e){
+            Log.d("Error", "IO Error");
+        }
+        finally{
             try {
                 writer.close();
             } catch (IOException e) {
-                Log.d("error", "File not found...");
+                Log.d("Error", "File not found...");
             }
+            Toast.makeText(this, "data saved...", Toast.LENGTH_LONG).show();
+
         }
-        Toast.makeText(this, "data saved...", Toast.LENGTH_LONG).show();
+
     }
 
-    public void goNext(View v) {
-        Intent i = new Intent(this, Checkbox.class);
+
+    public void goNext(View v){
+        Intent i = new Intent(getApplicationContext(), Checkbox.class);
         startActivity(i);
     }
 }
